396a7d3b0d21b88aa97af37d42506fd0955cff38
J. Duane Northcutt <duane.northcutt@gmail.com>
2017-09-19 15:17:23 -0700

    Checkpoint.

--- nestcam_capture.py
+++ nestcam_capture.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-####!/cygdrive/c/Python27/python
+#!/usr/bin/env python
 
 """Nest Cam Capture Tool"""
 
@@ -13,34 +12,39 @@
 
 
 import argparse
+import collections
 from datetime import datetime
 import glob
 import json
 import os
-import requests
 import sys
 import time
+
+import nestcam
 import yaml
 
 
 # N.B. Without a NestAware subscription, Google limits snapshots to 2 per minute (per-camera or per-site?)
 GOOGLE_RATE_LIMIT = 30 * 1000   # 30 secs
 
-# default configuraton
+# Initalize the default configuraton
 config = {
     "testing": True,
-    "delay": 10 * 60,   # 10 mins
-    "maxFrames": 10,    # keep last 10 frames
-    "numFrames": 0,	    # capture forever
-    "outPath": "/tmp/imgs/"  # save frames in /tmp/imgs/<camName>/<time>.jpg
+    "cameraNames": [],    # use all cameras
+    "delay": 10 * 60,     # 10 mins in between captures
+    "maxFrames": 10,      # keep last 10 frames
+    "numFrames": 0,	      # capture forever
+    "outputPath": "/tmp/imgs/",  # save frames in /tmp/imgs/<camName>/<time>.jpg
+    "productId": None,    # required
+    "productSecret": None # required
 }
 
 
 # Merge a new dict into an old one, updating the old one (recursively).
 def dictMerge(old, new):
-    for k, v in new.iteritems():
+    for k in new.keys():
         if (k in old and isinstance(old[k], dict) and
-            isinstance(new[k], collections.Mapping)):
+                isinstance(new[k], collections.Mapping)):
             dictMerge(old[k], new[k])
         else:
             old[k] = new[k]
@@ -56,20 +60,22 @@ def main():
         sys.stderr.write("Usage: {0}\n".format(usage))
         sys.exit(1)
 
-    usage = sys.argv[0] + "[-v] [-Q] [-c <confFile>]"
-    '''
-        "[-n <names>] [-c <confFile>] " + \
-        "[-d <secs>] [-u <username>] [-p <passwd>] " + \
-        "[-f <numFrames>] [-m <maxFrames>] [-o <outPath>]"
-    '''
+    usage = sys.argv[0] + "[-v] [-L] [-S [-Q <query>]] [-n <names>] " + \
+        "[-c <confFile>] [-d <secs>] [-f <numFrames>] [-m <maxFrames>] " + \
+        "[-o <outPath>] [-p <productId>] [-s <secret>]"
     ap = argparse.ArgumentParser()
     ap.add_argument(
-        '-Q', '--query', action='store_true',
-        help="list info on available cameras (and return)")
-    '''
+        '-L', '--list', action='store_true', default=False,
+        help="list info on selected cameras (and return)")
     ap.add_argument(
-        '-n', '--names', action='store', type=str,
-        help="comma-separated list of camera names")
+        '-S', '--status', action='store_true', default=False,
+        help="print info for the selected cameras (and don't capture images)")
+    ap.add_argument(
+        '-Q', '--query', action='store', type=str,
+        help="jq-like query string to apply to Status output (defaults to '.' if not given")
+    ap.add_argument(
+        '-c', '--configFile', action='store',
+        help="configuration input file path (defaults to './nestcam.conf'")
     ap.add_argument(
         '-d', '--delay', action='store', type=int,
         help="number of seconds to delay between sets of image grabs")
@@ -80,18 +86,17 @@ def main():
         '-m', '--maxFrames', action='store', type=int,
         help="maximum number of frames to save")
     ap.add_argument(
-        '-o', '--outPath', action='store', type=str,
-        help="path to directory where image grabs are to be written")
+        '-n', '--names', action='store', type=str,
+        help="comma-separated list of camera names")
     ap.add_argument(
-        '-u', '--user', action='store', type=str,
-        help="user name")
+        '-o', '--outputPath', action='store', type=str,
+        help="base directory for output image files")
     ap.add_argument(
-        '-p', '--passwd', action='store', type=str,
-        help="password")
-    '''
+        '-p', '--productId', action='store', type=str,
+        help="Nest Home product ID")
     ap.add_argument(
-        '-c', '--configFile', action='store',
-        help="configuration input file path (defaults to './nestcam.conf'")
+        '-s', '--secret', action='store', type=str,
+        help="Nest Home product secret")
     ap.add_argument(
         '-v', '--verbose', action='count', default=0,
         help="increase verbosity")
@@ -115,39 +120,24 @@ def main():
     if confFilePath is not None:
         with open(confFilePath, 'r') as ymlFile:
             confFile = yaml.load(ymlFile)
-        dictMerge(config, confFile)
+        if confFile:
+            dictMerge(config, confFile)
 
     # overwrite values from defaults and config file with cmd line options
-    if options.user:
-        config['user'] = options.user
-    if options.passwd:
-        config['passwd'] = options.passwd
+    if options.names:
+        config['cameraNames'] = options.names.strip().split(",")
     if options.delay:
         config['delay'] = options.delay
     if options.numFrames:
         config['numFrames'] = options.numFrames
     if options.maxFrames:
         config['maxFrames'] = options.maxFrames
-    if options.outPath:
-        config['outPath'] = options.outPath
-    if options.verbose > 1:
-        print("Configuration:")
-        json.dump(config, sys.stdout, indent=4, sort_keys=True)
-        print("")
-
-    # get the set of cameras to deal with
-    cameras = config['cameras'].values()
-    if options.names:
-        cams = options.names.split(',')
-        camNames = config['cameras'].keys()
-        cameras = []
-        for cam in cams:
-            if cam in camNames:
-                cameras.append(config['cameras'][cam])
-            else:
-                fatalError("Non-existant camera '{0}'".format(cam))
-    if options.verbose > 1:
-        print("Camera Ids: {0}".format(cameras))
+    if options.outputPath:
+        config['outputPath'] = options.outputPath
+    if options.productId:
+        config['productId'] = options.productId
+    if options.secret:
+        config['productSecret'] = options.secret
 
     # validate config values
     if config['numFrames'] < 0:
@@ -156,73 +146,98 @@ def main():
         fatalError("Number of frames to retain must be non-negative")
     if config['delay'] < 0:
         fatalError("Inter-frame delay must be non-negative")
-    if not config['outPath']:
+    if not config['outputPath']:
         fatalError("Must provide output path")
+    if not config['productId'] or not config['productSecret']:
+        fatalError("Must provide Nest Home product ID and Secret")
 
-    # instantiate the interface object and all of the chosen camera objects
+    # instantiate the NestCam interface object
     tries = 3
     while tries > 0:
         try:
-            nest = NestCam(config['user'], config['passwd'], cameras)
+            nest = nestcam.NestAccount(config['productId'], config['productSecret'])
             break
         except Exception as e:
             if options.verbose > 0:
-                sys.stderr.write("Warning: Failed to initialize nestcam: {0}".
+                sys.stderr.write("Warning: Failed to attach to NestCam server: {0}".
                                  format(e))
         tries -= 1
     if tries <= 0:
-        fatalError("Unable to initalize nestcam")
-    cams = nest.cameras()
+        fatalError("Unable to attach to NestCam server")
 
-    if not os.path.exists(config['outPath']):
-        os.makedirs(config['outPath'])
-    for cam in cams:
-        camName = cam.name()
-        path = os.path.join(config['outPath'], camName)
+    # get ids for all of the selected cameras
+    if not config['cameraNames']:
+        config['cameraNames'] = nest.cameraNames()
+        config['cameraIds'] = nest.cameraIds()
+    else:
+        config['cameraIds'] = []
+        for camName in config['cameraNames']:
+            camIds = nest.cameraIdLookup(camName)
+            if camIds is None:
+                fatalError("Non-existant camera '{0}'".format(camName))
+            if len(camIds) != 1:
+                fatalError("Ambiguous camera name '{0}': {1}".format(camName, camIds))
+            config['cameraIds'].append(camIds[0])
+
+    # validate and init the directories for all of the cameras' images
+    if not os.path.exists(config['outputPath']):
+        os.makedirs(config['outputPath'])
+    for camId, camName in nest.camerasNameMap().iteritems():
+        path = os.path.join(config['outputPath'], camName + camId)
         if not os.path.exists(path):
             os.makedirs(path)
 
-    if config['testing']:
-        # run tests and bail
-        camNames = nest.cameraNames()
-        print("CameraNames: {0}".format(camNames))
+    if options.verbose > 1:
+        print("Configuration:")
+        json.dump(config, sys.stdout, indent=4, sort_keys=True)
+        print("")
 
-        camIds = nest.cameraIds()
-        print("CameraIds: {0}".format(camIds))
+    # get the current state of all the cameras associated with this account
+    if options.verbose > 2:
+        allCamIds = nest.cameraIds()
+        print("All Camera Ids: {0}".format(allCamIds))
 
-        camsMap = nest.camerasMap()
-        print("CamerasMap: {0}".format(camsMap))
+        allCamNames = nest.cameraNames()
+        print("All Camera Names: {0}".format(allCamNames))
 
-        for cam in cams:
-            '''
-            print("Camera: {0} - {1}".format(cam.name(), cam.id()))
-            cam.dump()
-            '''
-            '''
-            capas = cam.capabilities()
-            print("Camera: {0}, capabilities={1}".format(cam.name(), capas))
-            '''
-            '''
-            for capa in capas:
-                val = cam.getCapability(capa)
-                print("Capability: {0}={1}".format(capa, val))
-            '''
-            events = cam.getEvents(0)
-            print("Events:")
-            json.dumps(events, sys.stdout, indent=4, sort_keys=True)
-        return
+        allCamsMap = nest.camerasNameMap()
+        print("Map of all Camera IDs to Names:")
+        json.dump(allCamsMap, sys.stdout, indent=4, sort_keys=True)
+        print("")
+
+    camerasInfo = {k: v for k, v in nest.cameras().iteritems() if k in config['cameraIds']}
+    if options.list:
+        print("Cameras Info:")
+        json.dump(camerasInfo, sys.stdout, indent=4, sort_keys=True)
+        print("")
+        sys.exit(0)
 
     # capture a frame from each camera in the list, writing the images to
     #  files in the given directory, wait the given amount of time, and repeat
     count = 0
     while True:
-        for cam in cams:
-            if options.verbose > 1:
-                print("Capturing from {0}".format(cam.name()))
+        for camId in config['cameraIds']:
+            info = nest.cameraInfo(camId)
+            name = info['name_long']
             ts = datetime.utcnow().isoformat()
+            if options.verbose > 2:
+                print("Timestamp: {0}".format(ts))
+            if options.status:
+                # get the status and don't capture an image
+                if options.verbose:
+                    print("Camera {0} Status:".format(name))
+                #### TODO if there's a query filter, apply it (else emit the whole thing)
+                json.dump(info, sys.stdout, indent=4, sort_keys=True)
+                continue
+
+            # capture an image
+            if options.verbose:
+                print("Capture image from camera {0}".format(name))
             try:
-                img = cam.grabFrame()
+                img = nest.getSnapshot(camId)
             except Exception:
+                continue
+            if not img:
                 continue
 
             # delete oldest frame if there are more than the max number of them
@@ -232,7 +247,7 @@ def main():
             if len(files) > config['maxFrames']:
                 files.sort()
                 try:
-                    if options.verbose > 1:
+                    if options.verbose > 2:
                         print("Removing file '{0}'".format(files[0]))
                     os.remove(files[0])
                 except Exception:
@@ -240,16 +255,19 @@ def main():
 
             fPath = os.path.join(camOutPath, ts + ".jpg")
             with open(fPath, "w+") as f:
-                if options.verbose > 1:
+                if options.verbose > 2:
                     print("Writing frame to file '{0}'".format(fPath))
                 f.write(img)
         if config['numFrames'] > 0:
             count += 1
             if count >= config['numFrames']:
-                if options.verbose > 2:
+                if options.verbose > 3:
                     print("Completed capture of {0} frames per camera".
                           format(count))
                 break
+        if options.verbose > 2:
+            print("Delaying {0} secs".format(config['delay']))
+            print("")
         time.sleep(config['delay'])
 
 if __name__ == '__main__':
